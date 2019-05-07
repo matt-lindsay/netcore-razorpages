8578662a60910f9e548b20b4c9464c2b429c8b7b
matt-lindsay <matt.lindsay@surreyheath.gov.uk>
2019-05-07 16:29:50 +0100

    Completed app

---
+++ RazorPagesMovie/Migrations/20190507141128_AddReviewerField.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class AddReviewerField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reviewer",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reviewer",
+                table: "Movie");
+        }
+    }
+}
